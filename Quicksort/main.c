7fa8dc4adbafd7d6f0e986194803c0e19bf83458
andreaba0 <andreabarchietto01@gmail.com>
2021-06-20 09:54:30 +0000

    Quicksort

--- Quicksort/main.c
+++ Quicksort/main.c
@@ -2,8 +2,21 @@
 #include<stdlib.h>
 #include<time.h>
 
+int partition(int*vet, int p, int r);
+void sort(int*vet, int p, int r);
+void swap(int*vet, int x, int y);
+
 void initArray(int*arr, int len) {
     for(int i =0;i<len;i++) arr[i]=rand()%100;
+}
+
+void displayArray(int*vet, int len) {
+    printf("Array: [");
+    for(int i=0;i<len;i++) {
+        printf("%d", vet[i]);
+        if(i<len-1) printf(", ");
+    }
+    printf(" ];\n");
 }
 
 int main(int argc, char*argv[]) {
@@ -19,6 +32,35 @@ int main(int argc, char*argv[]) {
     srand(time(NULL));
     int vet[len];
     initArray(vet, len);
-    
+    displayArray(vet, len);
+    sort(vet, 0, len-1);
+    displayArray(vet, len);
     return 0;
+}
+
+int partition(int*vet, int p, int r) {
+    int pivot = vet[(p+r)/2];
+    int pivotpos = (p+r)/2;
+    swap(vet, pivotpos, p);
+    pivotpos=p;
+    int i;
+    for(i=p+1;i<=r;i++)
+        if(vet[i]<pivot) swap(vet, ++pivotpos, i);
+    swap(vet, pivotpos, p);
+    return pivotpos;
+}
+
+void sort(int*vet, int p, int r) {
+    if(p>=r) return;
+    printf("%d->%d\n", p, r);
+    int q = partition(vet, p, r);
+    sort(vet, p, q-1);
+    sort(vet, q+1, r);
+}
+
+void swap(int*vet, int x, int y) {
+    int temp;
+    temp=vet[x];
+    vet[x]=vet[y];
+    vet[y]=temp;
 }
