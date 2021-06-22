0ab333715bf7f98b64e71cd1e87e8598dfaec196
andreaba0 <andreabarchietto01@gmail.com>
2021-06-22 07:21:07 +0000

    fix

--- EsTestFile/main.c
+++ EsTestFile/main.c
@@ -13,12 +13,21 @@ int main(int argc, char*argv[]) {
         printf("Impossibile aprire il file\n");
         return 1;
     }
+    rewind(file);
     char str[20];
     int ok;
     while(1) {
         ok=fscanf(file, "%19s", str);
         if(ok!=1) break;
         printf("Stringa: '%10s', len: %5d\n", str, (int)strlen(str));
+    }
+    rewind(file);
+    char c;
+    while(1) {
+        ok=fscanf(file, "%c", &c);
+        if(ok!=1) break;
+        if(c!='\n') printf("Carattere: '%c', ASCII: %d\n", c, c);
+        else printf("ASCII simbolo a capo: '%d'\n", c);
     }
     fclose(file);
     return 0;
